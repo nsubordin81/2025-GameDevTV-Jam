e9a5ecec1e3c81166d30fda803311d149649bd19
Taylor Bird <nsubordin81@gmail.com>
2025-05-23 15:39:59 -0400

    getting the skeleton to do what I want it to do and the player to mostly do that too. progress

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,20 @@
 class_name Player extends CharacterBody2D
 # this is important when we are trying to use the 'is' keyword for pattern matching
 
+signal hit
+signal die
+
 const SPEED = 300.0
 
 @export var jump_force = 200.0
 @export var gravity = 400.0
 @export var speed = 125.0
+@export var health = 100
 @onready var animated_sprite = $AnimatedSprite2D
+
+const SKELETON_NAME = "Skeleton"
+
+var recovering = false
 
 var active = true
 
@@ -20,7 +28,7 @@ func _physics_process(delta):
 		jump(jump_force)
 
 	var direction = 0
-	if active==true:
+	if active == true:
 		# terminal velocity
 		if velocity.y > 500.0:
 			velocity.y = 500.0
@@ -38,10 +46,43 @@ func _physics_process(delta):
 	# this is twhat actually moves the CharacterBody2D
 	move_and_slide()
 
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		# print(collision.get_collider().name)
+		
+		if collision.get_collider().name == SKELETON_NAME and not recovering:
+			print("collided with a skeleton!")
+			take_damage()
+
 	update_animations(direction)
 
 func jump(force: float):
-	velocity.y = -force
+	velocity.y = - force
+
+func dying():
+	print("you are dead")
+	die.emit()
+
+func take_damage():
+	hit.emit()
+	health = health - 5
+	print("your new health is ", health)
+	if health <= 0:
+		dying()
+	else:
+		recovering = true
+		start_recovering()
+
+func start_recovering():
+	var recovery_timer = Timer.new()
+	recovery_timer.wait_time = 2.0
+	recovery_timer.one_shot = true
+	recovery_timer.connect("timeout", _on_recovery_timeout)
+	add_child(recovery_timer)
+	recovery_timer.start()
+
+func _on_recovery_timeout():
+	recovering = false
 
 func update_animations(direction):
 	if is_on_floor():
@@ -51,4 +92,3 @@ func update_animations(direction):
 			animated_sprite.play("walk")
 	else:
 		animated_sprite.play("jump")
-
--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
-# Variables
-var health: int = 10
+# # Variables
+# var health: int = 10
 var direction: int = 1 # 1 for right, -1 for left
 var walk_time: float = 0.0
 var walk_timer: float = 0.0
 var speed: float = 100.0
 var gravity: float = 500.0 # Gravity value
 var previous_velocity = velocity
+var death_timer = 10.0 # Initialize death timer with a longer fuse (e.g., 10 seconds)
+var dead = false
 
 
 # Reference to the AnimatedSprite2D node
@@ -15,39 +17,70 @@ var previous_velocity = velocity
 
 # Called when the node enters the scene tree for the first time
 func _ready():
-    randomize_walk_time()
+	randomize_walk_time()
+
+	animated_sprite.animation_finished.connect(_on_animation_finished)
 
 # Called every frame
 func _physics_process(delta: float):
-    walk_timer -= delta
-    if walk_timer <= 0:
-        change_direction()
-    
-    # Apply gravity
-    velocity.y += gravity * delta
-    
-    # Set horizontal velocity
-    velocity.x = speed * direction
+	# Decrease the death timer and check if it has run out
+	self.death_timer -= delta
+	if self.death_timer <= 0:
+		death()
 
-    # if velocity != previous_velocity:
-        # print("Velocity: ", velocity)
-    previous_velocity = velocity
-    
-    # Move the skeleton
-    move_and_slide()
+	walk_timer -= delta
+	if walk_timer <= 0:
+		change_direction()
+	
+	# Apply gravity
+	velocity.y += gravity * delta
+	
+	# Set horizontal velocity
+	velocity.x = speed * direction
+
+	# if velocity != previous_velocity:
+		# print("Velocity: ", velocity)
+	previous_velocity = velocity
+	
+	# Move the skeleton
+	move_and_slide()
+
+	update_animations()
 
 # Randomize the walk time
 func randomize_walk_time():
-    walk_time = round(randf_range(0.0, 5.0)) # Random time between 1 and 5 seconds, rounded to a whole number
-    walk_timer = walk_time
-    # print("Walk timer set to: ", walk_timer)
+	walk_time = round(randf_range(0.0, 5.0)) # Random time between 1 and 5 seconds, rounded to a whole number
+	walk_timer = walk_time
+	# print("Walk timer set to: ", walk_timer)
 
 # Change direction and flip sprite
 func change_direction():
-    direction *= -1
-    animated_sprite.flip_h = direction == -1
-    randomize_walk_time()
+	direction *= -1
+	animated_sprite.flip_h = direction == -1
+	randomize_walk_time()
 
 # Spawn the skeleton at a specific location
 func spawn(location: Vector2):
-    global_position = location
+	global_position = location
+
+func death():
+	print("yo, the skeleton is dead")
+	set_physics_process(false)
+	dead = true
+
+func _on_animation_finished():
+	print("animation is done")
+	if animated_sprite.animation == "death":
+		print("and we are here")
+		queue_free()
+
+func update_animations():
+	if dead:
+		animated_sprite.play("death")
+	elif is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("walk")
+	else:
+		animated_sprite.play("jump")
