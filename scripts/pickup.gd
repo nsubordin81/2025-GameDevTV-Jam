ed4eeacac3e19a27e26416c0f32b2da407ee0a1c
Taylor Bird <nsubordin81@gmail.com>
2025-05-24 00:26:30 -0400

    finishing this part:

--- scripts/pickup.gd
+++ scripts/pickup.gd
@@ -5,9 +5,14 @@ signal hit
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _ready() -> void:
-    animated_sprite.play("idle")
+	animated_sprite.play("idle")
+	animated_sprite.animation_finished.connect(_on_animation_finished)
+
+func _on_animation_finished():
+	print("pot cracked")
+	if animated_sprite.animation == "crack":
+		queue_free()
 
 func combust():
-    hit.emit()
-    animated_sprite.play("crack")
-    queue_free()
+	hit.emit()
+	animated_sprite.play("crack")
